c9a25baf142df62d36343054f7e8c89b48af8726
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-08-13 14:34:35 -0300

    feat: Starting Headers

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Headers from "./components/Headers";
 
 function App() {
   return (
-    <main className="">
+    <main className="bg-[hsl(0, 0%, 100%)] w-screen h-screen">
       <Headers />
     </main>
   );
--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,23 +1,29 @@
 import React from "react";
+import cart from "../../images/icon-cart.svg";
+import avatar from "../../images/image-avatar.png";
 
 const Headers = () => {
   return (
-    <div className="flex justify-between w-[1440px] items-center p-4">
-      <div className="flex items-center space-x-4">
-        <h1>sneakers</h1>
+    <header className="flex justify-between w-[1440px] items-center py-8 border-b border-gray-300 m-auto ">
+      <nav className="flex items-center space-x-8 ">
+        <h1 className="font-bold text-2xl mb-2">sneakers</h1>
         <ul className="flex space-x-4">
-          <li>Collections</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li className="text-gray-400">Collections</li>
+          <li className="text-gray-400">Men</li>
+          <li className="text-gray-400">Women</li>
+          <li className="text-gray-400">About</li>
+          <li className="text-gray-400">Contact</li>
         </ul>
-      </div>
-      <ul>
-        <li>3</li>
-        <li>4</li>
+      </nav>
+      <ul className="flex space-x-8">
+        <li>
+          <img src={cart} className="w-[15px] h-[15px] mt-1" alt="" />
+        </li>
+        <li className="">
+          <img src={avatar} className="w-[25px] h-[25px]" alt="" />
+        </li>
       </ul>
-    </div>
+    </header>
   );
 };
 
