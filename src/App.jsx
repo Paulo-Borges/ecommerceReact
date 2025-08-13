b8e709f79764d86351b84e866029d4bcab2b61df
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-08-13 20:53:02 -0300

    feat: started home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,64 @@
 import Headers from "./components/Headers";
+import productPrincipal from "../images/image-product-1.jpg";
+import product1 from "../images/image-product-1-thumbnail.jpg";
+import product2 from "../images/image-product-2-thumbnail.jpg";
+import product3 from "../images/image-product-3-thumbnail.jpg";
+import product4 from "../images/image-product-4-thumbnail.jpg";
+import cart from "../images/icon-cart.svg";
 
 function App() {
   return (
-    <main className="bg-[hsl(0, 0%, 100%)] w-screen h-screen">
+    <div className="bg-[hsl(0, 0%, 100%)] w-screen h-screen">
       <Headers />
-    </main>
+      <section className="flex w-[1350px] justify-around m-auto pt-15">
+        <div>
+          <img src={productPrincipal} className="w-[450px] rounded-md" alt="" />
+          <ul className="flex w-[450px] space-x-4 mt-4">
+            <li>
+              <img src={product1} className="rounded-md" alt="" />
+            </li>
+            <li>
+              <img src={product2} className="rounded-md" alt="" />
+            </li>
+            <li>
+              <img src={product3} className="rounded-md" alt="" />
+            </li>
+            <li>
+              <img src={product4} className="rounded-md" alt="" />
+            </li>
+          </ul>
+        </div>
+        <main className="w-[600px] mt-15">
+          <p>SNEAKER COMPANY</p>
+          <h1 className="text-5xl font-medium mb-7 mt-5">
+            Fall Limited Edition Sneakers
+          </h1>
+          <p className="w-[500px] mb-7">
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they´ll whithstand everything
+            the weather can offer.
+          </p>
+          <div className="flex items-center gap-4 mb-4">
+            <span className="font-bold text-3xl">$125.00</span>
+            <span className="bg-black text-white text-sm py-1 px-2 rounded-md">
+              50%
+            </span>
+          </div>
+          <div className="font-bold line-through mb-7">$250.00</div>
+          <div className="flex gap-3">
+            <div className="flex gap-4 bg-gray-200 w-[100px] rounded-md justify-center items-center">
+              <button className="text-orange-400 font-bold">-</button>
+              <button className="font-bold mt-1">0</button>
+              <button className="text-orange-400 font-bold">+</button>
+            </div>
+            <button className="flex gap-3 bg-amber-500 py-3 justify-center w-[300px] rounded-md">
+              <img src={cart} className="w-[15px] h-[15px] mt-0.5" alt="" />
+              <p className="text-black font-bold">Add to cart</p>
+            </button>
+          </div>
+        </main>
+      </section>
+    </div>
   );
 }
 
--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -4,23 +4,41 @@ import avatar from "../../images/image-avatar.png";
 
 const Headers = () => {
   return (
-    <header className="flex justify-between w-[1440px] items-center py-8 border-b border-gray-300 m-auto ">
+    <header className="flex justify-between w-[1440px] max-[1439px]:w-[1100px] items-center py-5 border-b border-gray-300 m-auto ">
       <nav className="flex items-center space-x-8 ">
         <h1 className="font-bold text-2xl mb-2">sneakers</h1>
         <ul className="flex space-x-4">
-          <li className="text-gray-400">Collections</li>
-          <li className="text-gray-400">Men</li>
-          <li className="text-gray-400">Women</li>
-          <li className="text-gray-400">About</li>
-          <li className="text-gray-400">Contact</li>
+          <li className="text-gray-400 hover:text-gray-400/60 cursor-pointer">
+            Collections
+          </li>
+          <li className="text-gray-400 hover:text-gray-400/60 cursor-pointer">
+            Men
+          </li>
+          <li className="text-gray-400 hover:text-gray-400/60 cursor-pointer">
+            Women
+          </li>
+          <li className="text-gray-400 hover:text-gray-400/60 cursor-pointer">
+            About
+          </li>
+          <li className="text-gray-400 hover:text-gray-400/60 cursor-pointer">
+            Contact
+          </li>
         </ul>
       </nav>
       <ul className="flex space-x-8">
         <li>
-          <img src={cart} className="w-[15px] h-[15px] mt-1" alt="" />
+          <img
+            src={cart}
+            className="w-[15px] hover:w-[20px] h-[15px] hover:h-[20px] mt-1 cursor-pointer"
+            alt=""
+          />
         </li>
         <li className="">
-          <img src={avatar} className="w-[25px] h-[25px]" alt="" />
+          <img
+            src={avatar}
+            className="w-[25px] h-[25px] cursor-pointer"
+            alt=""
+          />
         </li>
       </ul>
     </header>
